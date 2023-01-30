20aab9f02721e95b365b1a621480798279e325f8
Ashutosh <ashutoshdubey852@gmail.com>
2023-01-30 21:08:58 +0530

    deletw all

--- bookStorev05/OnlineBookShop/src/main/java/com/onlinestore/services/exceptions/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.onlinestore.services.exceptions;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- bookStorev05/OnlineBookShop/src/test/java/com/onlinestore/data.sql
+++
@@ -1,1 +0,0 @@
-create table book_info (isbn bigint not null, author varchar(255), price float not null check (price>=0), quantity integer not null check (quantity>=0), sold integer not null check (sold>=0), title varchar(255) not null, version integer not null, primary key (isbn));
